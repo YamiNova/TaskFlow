05740fb95b7f15a821cfd46bc311e37e7b955cdc
xhy <hangyu_xia@outlook.com>
2026-04-19 23:44:26 +0200

    modify router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',           // URL 路径
+    name: 'home',        // 路由名字（可选）
+    component: HomeView  // 对应页面
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  history: createWebHistory(),
+  routes
 })
 
 export default router
